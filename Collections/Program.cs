9f690664ce1add3464d22906f97b436d039c7a14
Dilay Berin Akkan <dilayakkan20@gmail.com>
2024-10-28 12:47:58 +0300

    text

---
+++ Collections/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler2 = new List<string> {"Engin","Murat","Kerem","Halil" };
+            isimler2.Add("İlker");
+        }
+
+    }
+}
